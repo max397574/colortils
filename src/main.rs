919ef0057629410062ca43d98373ff34c7c5033c
max397574 <max.heinzer397@gmx.ch>
2022-05-27 14:20:28 +0200

    feat: move lighten and darken functions into mod

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,9 @@
+mod color_utils;
+
 // eventually use: https://crates.io/crates/anyhow/1.0.57
 use ansi_term::{ANSIString, ANSIStrings, Colour::RGB, Style};
 use regex::Regex;
 use std::{io, u8};
-
-fn darken_color(color: &(u8, u8, u8), amount: u8) -> (u8, u8, u8) {
-    let new_red: u8;
-    let new_blue: u8;
-    let new_green: u8;
-    new_red = if amount < color.0 {
-        color.0 - amount
-    } else {
-        0
-    };
-    new_blue = if amount < color.1 {
-        color.1 - amount
-    } else {
-        0
-    };
-    new_green = if amount < color.2 {
-        color.2 - amount
-    } else {
-        0
-    };
-    (new_red, new_blue, new_green)
-}
-
-fn lighten_color(color: &(u8, u8, u8), amount: u8) -> (u8, u8, u8) {
-    let new_red: u8;
-    let new_blue: u8;
-    let new_green: u8;
-    new_red = if 255 - amount > color.0 {
-        color.0 + amount
-    } else {
-        255
-    };
-    new_blue = if 255 - amount > color.1 {
-        color.1 + amount
-    } else {
-        255
-    };
-    new_green = if 255 - amount > color.2 {
-        color.2 + amount
-    } else {
-        255
-    };
-    (new_red, new_blue, new_green)
-}
 
 fn main() {
     let colors_re = Regex::new(r"#?([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})").unwrap();
@@ -126,9 +84,9 @@ fn main() {
         .expect("Not a number between 0 and 255");
 
     if action == "l" {
-        new_colors = lighten_color(&color_tup, amount_int);
+        new_colors = color_utils::adjusting::lighten_color(&color_tup, amount_int);
     } else {
-        new_colors = darken_color(&color_tup, amount_int);
+        new_colors = color_utils::adjusting::darken_color(&color_tup, amount_int);
     }
 
     let light_strings: &[ANSIString<'static>] = &[
